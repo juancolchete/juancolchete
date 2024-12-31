c9602158198b6be6bf9b618c03e8db2a292c1fdc
juancolchete <juanudkp@gmail.com>
2024-12-31 14:47:04 -0300

    feat: esp32 fix request

--- src/pages/api/esp32.ts
+++ src/pages/api/esp32.ts
@@ -4,12 +4,11 @@ import axios from 'axios'
 type ResponseData = {
   message: string
 }
- 
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-const { output, state } = req.query
-  axios.get(`${process.env.ESP32URL}/update?output=${output}&state=${state}`)
+  axios.get(`${process.env.ESP32URL}/update?output=${req.query.output}&state=${req.query.state}`)
   res.status(200).json({ message: 'OK' })
 }
