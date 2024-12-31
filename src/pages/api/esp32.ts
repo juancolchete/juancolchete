65d2fcf1396cce8a8c7a414b8153cd58dbc7f55d
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2024-12-31 17:18:22 +0000

    feat: ESP32 API URL

---
+++ src/pages/api/esp32.ts
@@ -0,0 +1,14 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
+type ResponseData = {
+  message: string
+}
+ 
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+const { output, state } = req.query
+  axios.get(`${process.env.ESP32URL}/update?output=${output}&state=${state}`)
+  res.status(200).json({ message: 'OK' })
+}
